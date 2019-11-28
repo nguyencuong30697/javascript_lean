d5aca3f774bbbfc7cd6bd269625326f85dd6c600
NguyenCuong <nguyencuong.3061997@gmail.com>
2019-11-28 15:33:43 +0700

    Update quyetde

---
+++ Javascript_demo1/Intro.js
@@ -0,0 +1,37 @@
+// var
+// let
+// const
+
+
+const myString = 'ABC';
+const my = "ABC1";
+const myNew = `abc ${my}`;
+
+const number = 4;
+
+
+const myFunction = (a, b, c) => {
+      
+};
+
+const myArray = [1,2,3,4];
+
+const firstItem = myArray[0];
+const lengthArray = myArray.length;
+
+const Aobject = {
+    name : `abc`,
+    age : 21,
+    adress : {
+        city :`HN`,
+        country :`VN`
+    },
+};
+
+
+const myName = Aobject.name;
+const myCountry = Aobject.adress.country;
+Aobject.name = `ABCD`;
+
+
+Console.log(myName);
